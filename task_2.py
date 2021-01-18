8551a0b7f013121ff7ea1008e31be241b5903810
Султангулов Данис Радикович <sultangulovdr-bsh@rosneft.ru>
2021-01-18 11:51:43 +0500

    task 2 final

--- task_2.py
+++ task_2.py
@@ -1,7 +1,6 @@
 '''
 Задание 2
-Создайте класс для конвертирования температуры из
-Цельсия в Фаренгейт и наоборот.
+Создайте класс для конвертирования температуры из Цельсия в Фаренгейт и наоборот.
 У класса должно быть два статических метода: для перевода из Цельсия в Фаренгейт и для перевода из Фаренгейта в Цельсий.
 Также класс должен считать количество подсчетов температуры и возвращать это значение с помощью статического метода.
 '''
@@ -11,27 +10,22 @@ from typing import List
 
 class Converter:
     __report: List[float] = []
+    __count: int = 0
+    far: float
+    celsius: float
 
-    def __init__(self, celsius: float):
-        self.celsius = celsius
-        # self.count += 1
-        self.__report.append(celsius)
+    def __init__(self):
+        Converter.__count += 1
 
-    def set_cel(self, value):
-        self.celsius = value
+    @staticmethod
+    def cel_to_far() -> float:
+        return Converter.celsius * 9 / 5 + 32
 
-    def set_far(self, value):
-        self.far = value
+    @staticmethod
+    def far_to_cel() -> float:
+        return (Converter.far - 32) * 5 / 9
 
-    def cel_to_far(self) -> float:
-        return cls.celsius * 9/5 + 32
-
-    @classmethod
-    def get_report(cls) -> List[float]:
-        return cls.__report
-
-
-temp1 = Converter(30.0)
-assert temp1.cel_to_far() == 86.0
-
+    @staticmethod
+    def get_count() -> int:
+        return Converter.__count
 
